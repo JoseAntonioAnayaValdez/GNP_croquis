9aa0d63f06946b606b84851b0a9a9e52975a2de7
Antonio <l21590402@sjuanrio.tecnm.mx>
2025-02-27 00:02:59 -0600

    Initialize Flask application

---
+++ appAntonio.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
