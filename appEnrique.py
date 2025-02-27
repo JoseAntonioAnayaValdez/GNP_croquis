8ab936e38b5db1020cf44f46b83533e92510fce7
Enrique <l21590309@sjuanrio.tecnm.mx>
2025-02-27 00:42:57 -0600

    croquis

---
+++ appEnrique.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/simulacion')
+def index():
+    return render_template('croquis.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
