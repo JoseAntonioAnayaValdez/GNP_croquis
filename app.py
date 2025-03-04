f104849ace985269fef93a9a75641eecdac0c6fc
Antonio <l21590402@sjuanrio.tecnm.mx>
2025-03-03 18:11:18 -0600

    Update .gitignore, add new route for simulation,

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/simulacion')
+def croquis():
+    return render_template('croquis.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- appAntonio.py
+++ appAntonio.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-import os
+# from flask import Flask, render_template, request, redirect, url_for
+# from flask_sqlalchemy import SQLAlchemy
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv()
-app = Flask(__name__)
+# load_dotenv()
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# @app.route('/simulacion')
+# def index():
+#     return render_template('croquis.html')
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
